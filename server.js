c4b266040c7637d60c4e2b892a6616d90446f431
Daniel Rosell <10818338@my.uvu.edu>
2022-10-26 18:40:23 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 8000
 
 app.use(express.static('client'))
 
-app.get('./client/index.js', (req, res) => {
+app.get('/todos', (req, res) => {
   res.send(todoModel)
   console.log(req)
 })
@@ -30,3 +30,38 @@ app.post('/todoModel', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+let todoModel = [
+  {
+      todoName: "Work in Todo App",
+      todoStatus: false,
+      todoId: 0,
+      todoCategory: "School",
+      todoDate: "09/09/22",
+      todoDailyReminder: false,
+  },
+  {
+      todoName: "Go to Work",
+      todoStatus: true,
+      todoId: 1,
+      todoCategory: "Work",
+      todoDate: "09/10/22",
+      todoDailyReminder: true,
+  },
+  {
+      todoName: "Look Up Cybersecurity",
+      todoStatus: false,
+      todoId: 2,
+      todoCategory: "Future Profession",
+      todoDate: "09/11/22",
+      todoDailyReminder: true,
+  },
+  {
+      todoName: "Look Up insertAdjacentHTML",
+      todoStatus: true,
+      todoId: 3,
+      todoCategoryId: 0,
+      todoDate: "09/18/22",
+      todoDailyReminder: false,
+  }
+]
